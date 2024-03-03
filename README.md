72bcb57620c15b3a50a3d1b847660c501fca82cf
shyamajp <shyamajp@gmail.com>
2024-03-04 00:44:17 +0200

    refactor Card and fix joker bug

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TODOs
 
-- [ ] fix a joker being paired with an ace
+- [x] fix a joker being paired with an ace
 - [] improve judge cases
   - [x] A picked one is your last card's pair
   - [] Your last card is picked (currently judged at the next round)
--- src/Card.ts
+++ src/Card.ts
@@ -1,11 +1,12 @@
-export enum SUITES {
+enum Suite {
   CLUBS,
   DIAMONDS,
   HEARTS,
   SPADES,
+  NONE,
 }
 
-export enum SUITES_SYMBOLS {
+enum SuiteSymbols {
   "♣",
   "♦",
   "♥",
@@ -13,29 +14,33 @@ export enum SUITES_SYMBOLS {
 }
 
 export class Card {
-  readonly card: number;
-  readonly suite: SUITES;
+  readonly raw: number;
+  readonly suite: Suite | string;
   readonly num: number | string;
 
-  constructor(c: number) {
-    this.card = c;
-    this.suite = Math.floor(c / 13);
-    this.num = (c % 13) + 1;
+  constructor(raw: number) {
+    this.raw = raw;
+    this.suite = this.getSuite();
+    this.num = this.getNumber();
   }
 
   getSuite(pretty: boolean = false) {
-    if (pretty) {
-      return SUITES_SYMBOLS[this.suite];
+    if (this.raw > 51) {
+      return "";
     }
-    return this.suite;
+    const suite = Math.floor(this.raw / 13);
+    if (pretty) {
+      return SuiteSymbols[suite];
+    }
+    return Suite[suite];
   }
 
-  getNumber(pretty: boolean = false): number | string {
-    if (this.card > 51) {
+  getNumber(pretty: boolean = false) {
+    if (this.raw > 51) {
       return "Joker";
     }
     if (pretty) {
-      switch ((this.card % 13) + 1) {
+      switch ((this.raw % 13) + 1) {
         case 1:
           return "A";
         case 11:
@@ -46,15 +51,12 @@ export class Card {
           return "K";
       }
     }
-    return (this.card % 13) + 1;
+    return (this.raw % 13) + 1;
   }
 
   display() {
     const n = this.getNumber(true);
     const suite = this.getSuite(true);
-    if (n === "Joker") {
-      return "Joker";
-    }
     return `${suite}${n}`;
   }
 }
