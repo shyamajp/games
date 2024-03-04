e14061c814379224a2c185ca9de7f62a9dd76f1b
shyamajp <shyamajp@gmail.com>
2024-03-04 22:29:40 +0200

    function overloading and lint

--- src/Card.ts
+++ src/Card.ts
@@ -6,57 +6,56 @@ enum Suite {
   NONE,
 }
 
-enum SuiteSymbols {
+enum SuiteSymbol {
   "♣",
   "♦",
   "♥",
   "♠",
 }
 
+enum SpecialCard {
+  A = 1,
+  J = 11,
+  Q = 12,
+  K = 13,
+  Joker = 52,
+}
+
+type PrettySuite = keyof typeof SuiteSymbol;
+type PrettyCard = keyof typeof SpecialCard;
+
 export class Card {
-  readonly raw: number;
-  readonly suite: Suite | string;
-  readonly num: number | string;
+  readonly raw;
+  readonly suite;
+  readonly num;
 
   constructor(raw: number) {
     this.raw = raw;
-    this.suite = this.getSuite();
-    this.num = this.getNumber();
+    this.suite = this.getSuite(false);
+    this.num = this.getNumber(false);
   }
 
+  getSuite(pretty: false): Suite | undefined;
+  getSuite(pretty: true): PrettySuite | undefined;
   getSuite(pretty: boolean = false) {
-    if (this.raw > 51) {
-      return "";
-    }
-    const suite = Math.floor(this.raw / 13);
-    if (pretty) {
-      return SuiteSymbols[suite];
-    }
-    return Suite[suite];
+    if (this.raw > 51) return undefined;
+    let suite: Suite | PrettySuite = Math.floor(this.raw / 13);
+    if (pretty) suite = SuiteSymbol[suite] as PrettySuite;
+    return suite;
   }
 
+  getNumber(pretty: false): number | Extract<PrettyCard, "Joker">;
+  getNumber(pretty: true): number | PrettyCard;
   getNumber(pretty: boolean = false) {
-    if (this.raw > 51) {
-      return "Joker";
-    }
-    if (pretty) {
-      switch ((this.raw % 13) + 1) {
-        case 1:
-          return "A";
-        case 11:
-          return "J";
-        case 12:
-          return "Q";
-        case 13:
-          return "K";
-      }
-    }
-    return (this.raw % 13) + 1;
+    if (this.raw >= SpecialCard.Joker) return SpecialCard[SpecialCard.Joker];
+    let num: number | PrettyCard = (this.raw % 13) + 1;
+    if (pretty && num in SpecialCard) num = SpecialCard[num] as PrettyCard;
+    return num;
   }
 
   display() {
-    const n = this.getNumber(true);
-    const suite = this.getSuite(true);
-    return `${suite}${n}`;
+    const num = this.getNumber(true);
+    const suite = this.getSuite(true) ?? "";
+    return `${suite}${num}`;
   }
 }
