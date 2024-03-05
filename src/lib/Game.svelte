6ad9f2590c5b20d88798ee12962b7cd6bfb072e6
shyamajp <shyamajp@gmail.com>
2024-03-05 22:36:04 +0200

    format

--- src/lib/Game.svelte
+++ src/lib/Game.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-import { Card } from "../Card";
-import { Deck } from "../Deck";
-import { OldMaid } from "../OldMaid";
-import { Player, PlayerStatus } from "../Player";
+  import { Card } from "../Card";
+  import { Deck } from "../Deck";
+  import { OldMaid } from "../OldMaid";
+  import { Player, PlayerStatus } from "../Player";
 
-const deck = new Deck(1);
+  const deck = new Deck(1);
 
-let game = new OldMaid(deck, [
-  new Player("Alice"),
-  new Player("Bob"),
-  new Player("Charlie"),
-]);
+  let game = new OldMaid(deck, [
+    new Player("Alice"),
+    new Player("Bob"),
+    new Player("Charlie"),
+  ]);
 
-function handleClick() {
+  function handleClick() {
     game.next();
     game = game;
-}
-
+  }
 </script>
 
 <button on:click={handleClick}>
@@ -24,7 +23,11 @@ function handleClick() {
 </button>
 
 <ul>
-    {#each game.players as player}
-        <li>{player.name}: ({PlayerStatus[player.status]}) - {player.cards.map((c) => new Card(c).display()).join(' | ')}</li>
-    {/each}
+  {#each game.players as player}
+    <li>
+      {player.name}: ({PlayerStatus[player.status]}) - {player.cards
+        .map((c) => new Card(c).display())
+        .join(" | ")}
+    </li>
+  {/each}
 </ul>
