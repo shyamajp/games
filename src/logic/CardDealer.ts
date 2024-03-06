34766fc0e145baa8a4ad5daf742af9b20efc84f9
shyamajp <shyamajp@gmail.com>
2024-03-06 23:10:30 +0200

    add id to CardDealer

--- src/logic/CardDealer.ts
+++ src/logic/CardDealer.ts
@@ -1,9 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
 import { getRandomElement } from "./utils";
 
 export abstract class CardDealer {
   cards: number[];
+  id: string;
 
   constructor() {
+    this.id = uuidv4();
     this.cards = [];
   }
 
