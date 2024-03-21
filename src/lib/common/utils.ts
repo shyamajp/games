8127f845e4c996bc95dbaf066a7bad82f267f315
shyamajp <shyamajp@gmail.com>
2024-03-21 22:20:16 +0200

    run prettier

--- src/lib/common/utils.ts
+++ src/lib/common/utils.ts
@@ -1,7 +1,7 @@
 function limitCheckbox(name: string, count: number) {
   const checkboxes = document.getElementsByName(name);
   const checked = [...checkboxes].filter(
-    (c: HTMLElement) => (c as HTMLInputElement).checked
+    (c: HTMLElement) => (c as HTMLInputElement).checked,
   );
   if (checked.length > count) {
     return true;
