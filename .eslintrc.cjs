9ddf5fc4f130d0efc827f19c75514c44fc1954df
shyamajp <shyamajp@gmail.com>
2024-03-23 17:10:01 +0200

    remove rules from eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,41 +7,6 @@ module.exports = {
     project: "tsconfig.json",
     extraFileExtensions: [".svelte"],
   },
-  rules: {
-    /* Possible Errors */
-    "svelte/infinite-reactive-loop": "warn",
-    "svelte/no-dom-manipulating": "error",
-    "svelte/no-dupe-on-directives": "error",
-    "svelte/no-dupe-use-directives": "error",
-    "svelte/no-reactive-reassign": "warn",
-    "svelte/no-store-async": "warn",
-    "svelte/no-inline-styles": "error",
-    /* Security Vulnerability */
-    "svelte/no-target-blank": "error",
-    /* Best Practices */
-    "svelte/button-has-type": "error",
-    "svelte/no-inline-styles": "error",
-    "svelte/no-reactive-functions": "warn",
-    "svelte/no-reactive-literals": "warn",
-    "svelte/prefer-destructured-store-props": "warn",
-    "svelte/require-each-key": "warn",
-    "svelte/require-optimized-style-attribute": "error",
-    "svelte/require-stores-init": "warn",
-    "svelte/valid-each-key": "error",
-    /* Stylistic Issues */
-    "svelte/html-closing-bracket-spacing": "error",
-    "svelte/html-quotes": "error",
-    "svelte/html-self-closing": "error",
-    "svelte/indent": "error",
-    "svelte/no-extra-reactive-curlies": "error",
-    "svelte/no-spaces-around-equal-signs-in-attribute": "error",
-    "svelte/shorthand-attribute": "error",
-    "svelte/shorthand-directive": "error",
-    "svelte/sort-attributes": "error",
-    "svelte/spaced-html-comment": "error",
-    /* Extension Rules */
-    "svelte/no-trailing-spaces": "error",
-  },
   overrides: [
     {
       files: ["*.svelte"],
