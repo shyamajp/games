1e2e4b91ddd7f19fe12f8f54a11caa3d69da5417
shyamajp <shyamajp@gmail.com>
2024-03-23 23:12:00 +0200

    remove unused

--- src/logic/common/utils.ts
+++ src/logic/common/utils.ts
@@ -1,13 +1,3 @@
 export function getRandomElement<T>(arr: T[]): T {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
-export function reorderArray<T>(arr: T[], index: number): T[] {
-  if (index < 0) {
-    throw new Error("Given index does not exist in its array.");
-  }
-  if (arr.length < index) {
-    throw new Error("Given index is bigger than its array length.");
-  }
-  return arr.concat(arr).slice(index, index + arr.length);
-}
